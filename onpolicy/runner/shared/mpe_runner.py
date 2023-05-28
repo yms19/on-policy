f8f4389522e41bf874d0d1bd9844bdd02a54d7a0
yms19 <371071523@qq.com>
2023-05-28 14:59:02 +0000

    fix get_good_action bug

--- onpolicy/runner/shared/mpe_runner.py
+++ onpolicy/runner/shared/mpe_runner.py
@@ -119,7 +119,7 @@ def get_good_action(num_agents, obs, agent_id, step):
     adv_y = 5
     init_pos = [[-0.05, 0], [0.05, 0], [0, -0.05], [0, 0.05], [0, 0], [0, 0], [0, 0], [0, 0]]
     # target_pos = [[0.5, 0.5], [0.67, 0.67], [0.7, 0.4], [1, 0.8]]
-    target_pos = [[obs[2+num_agents*4], obs[3+num_agents*4]]] * 8
+    target_pos = [[obs[3+num_agents*4], obs[4+num_agents*4]]] * 8
     action_env = np.zeros(shape=(1, 7))
     detect_threshold = 0.02
     if obs[adv_x] == 0 and obs[adv_y] == 0:
@@ -141,7 +141,7 @@ def get_good_action_with_detect(num_agents, obs, agent_id, step, avail_action):
     adv_x = 4
     adv_y = 5
     init_pos = [[-0.05, 0], [0.05, 0], [0, -0.05], [0, 0.05], [0, 0], [0, 0], [0, 0], [0, 0]]
-    adv_center = [obs[2+num_agents*4], obs[3+num_agents*4]]
+    adv_center = [obs[3+num_agents*4], obs[4+num_agents*4]]
     target_pos1 = [adv_center, [adv_center[0]+0.17, adv_center[1]+0.17], [adv_center[0]+0.5, adv_center[1]-0.1], [adv_center[0]+0.5, adv_center[1]+0.3]]
     target_pos2 = [[adv_center[0]-0.2, adv_center[1]], [adv_center[0]+0.17, adv_center[1]+0.57], [adv_center[0]+0.7, adv_center[1]-0.1], [adv_center[0]+0.7, adv_center[1]+0.3]]
     action_env = np.zeros(shape=(1, 7))
